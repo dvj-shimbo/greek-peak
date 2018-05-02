43ce3d2f92d070917172b0a4ac7ee611cef64f01
dvj-shimbo <dvj.shimbo@gmail.com>
2018-05-03 01:59:30 +0200

    Adding Header Menu open/close script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,4 +5,85 @@ $(window).on("load", function() {
 
 	console.log("scripts.js is loaded");
 
+
+	$("<div class=\"device-phone\"></div>").appendTo("body");
+	$("<div class=\"device-phone-wide\"></div>").appendTo("body");
+	$("<div class=\"device-phablet\"></div>").appendTo("body");
+	$("<div class=\"device-tablet-small\"></div>").appendTo("body");
+	$("<div class=\"device-tablet\"></div>").appendTo("body");
+	$("<div class=\"device-tablet-wide\"></div>").appendTo("body");
+	$("<div class=\"device-desktop\"></div>").appendTo("body");
+	$("<div class=\"device-desktop-wide\"></div>").appendTo("body");
+
+
+
+
+	var  isBreakpoint = function(alias) {
+		return $(".device-" + alias).is(":visible");
+	};
+
+
+	
+
+
+
+	$("header").click(function(e) {
+
+//		console.log("Header Click");
+
+		if (!isBreakpoint("desktop-wide")) {
+
+//			console.log(e.pageX + "   " + e.pageY);
+
+			var posX = e.pageX;
+			var posY = e.pageY;
+
+			if (posX > 15 && posX < 53 && posY > 5 && posY < 43) {
+
+				
+
+				if ($("header.menu-displayed").length) {
+
+//					console.log ("Back");
+
+					$("header").removeClass("menu-displayed");
+
+				} else {
+
+//					console.log ("Burger");
+
+					$("header").addClass("menu-displayed");
+
+				}
+
+
+			}
+
+
+		}
+		
+	});
+
+
+
+
+
+
+
+
+	function redraw() {
+//		console.log("REDRAW");
+//		console.log(isBreakpoint("desktop-wide"));
+	}
+
+	$(window).resize(function(){
+		redraw();
+	});
+
+	redraw();
+
+	
+
+
+
 });
